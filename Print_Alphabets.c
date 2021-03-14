6c3e42fc9af2bed007b5a81060ae0717b3794e06
Vipul Pawar <73781883+VVPawar@users.noreply.github.com>
2021-03-14 12:45:24 +0530

    Create Print_Alphabets.c

---
+++ Print_Alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+  int i='a',j='z';
+  printf("Alphabets from a to z \n");
+  while(){
+    printf("%c\t",i);
+    i++;
+  }
+  getch();
+}
+
+/*Output:
+  Alphabets from a to z 
+  a    b     c     d     e     f     g    h    i     j
+  k    l     m     n     o     p     q    r    s     t
+  u    v     w     x     y     z  
+  */
